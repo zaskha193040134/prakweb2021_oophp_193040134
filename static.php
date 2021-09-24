2c168310bac4c81e3778578bc2374051020648b1
zaskha193040134 <193040134.zaskha@mail.unpas.ac.id>
2021-09-24 22:02:58 +0700

    mempelajari oophp dasar yaitu Static Keyword (bagian 2) (video-11)

--- static.php
+++ static.php
@@ -1,17 +1,39 @@
 <?php
 
-class ContohStatic {
+// class ContohStatic {
+//     public static $angka = 1;
+
+//     public static function halo(){
+//         return "Halo" . self::$angka++ . " kali";
+//     }
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+
+
+class Contoh {
     public static $angka = 1;
 
-    public static function halo(){
-        return "Halo" . self::$angka++ . " kali";
+    public function halo(){
+        return "Halo". self::$angka++ . " Kali";
     }
 }
 
-echo ContohStatic::$angka;
-echo "<br>";
-echo ContohStatic::halo();
+$obj = new Contoh();
+echo $obj->halo();echo "<br>";
+echo $obj->halo();echo "<br>";
+echo $obj->halo();echo "<br>";
+
 echo "<hr>";
-echo ContohStatic::halo();
+
+$obj2 = new Contoh();
+echo $obj2->halo();echo "<br>";
+echo $obj2->halo();echo "<br>";
+echo $obj2->halo();echo "<br>";
 
 ?>
